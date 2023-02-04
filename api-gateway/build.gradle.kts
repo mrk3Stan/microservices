0a3d93c2d12b353dda915ccf6ce5df07c717f0c5
Mark Bustillo <markbustillo@cryptoblk.io>
2023-02-04 19:03:59 +0800

    fix oauth2 dependency typo error

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -4,6 +4,6 @@ configurations.implementation {
 
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
-    implementation("org.springframework.boot:spring-boot-starter-oath2-resource-server")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-security")
 }
